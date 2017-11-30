30b2c1a550f781532307a9cd0b6903a6985a2b6b
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-11-30 17:41:22 -0500

    Configurando proyecto dia 9

--- Dia 9 - 14 Must know Dev Tools Tricks/javascript.js
+++ Dia 9 - 14 Must know Dev Tools Tricks/javascript.js
@@ -1,1 +1,38 @@
-console.log("Hi");
+
+const dogs = [
+    {
+        name: 'Snickers',
+        agge: 2
+    },
+    { name: 'hugo', age: 8 }
+];
+
+function makeGreen() {
+    const p = document.querySelector('p');
+    p.style.color = '#bada55';
+    p.style.fontSize = '50px';
+}
+
+    // Regular
+
+    // Interpolated
+
+    // Styled
+
+    // warning!
+
+    // Error :|
+
+    // Info
+
+    // Testing
+
+    // clearing
+
+    // Viewing DOM Elements
+
+    // Grouping together
+
+    // counting
+
+    // timing
