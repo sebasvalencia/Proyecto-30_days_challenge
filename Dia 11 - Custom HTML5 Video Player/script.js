f6a096312b29ed0b2fffd598b0f2397ed581b58a
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-12-02 17:40:47 -0500

    Creamos funcion manejar los rangos

--- Dia 11 - Custom HTML5 Video Player/script.js
+++ Dia 11 - Custom HTML5 Video Player/script.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 //Build out functions
 //Llamar a play o pause
@@ -34,6 +34,12 @@ function skip(){
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate(){
+    console.log(this.value);
+    console.log(this.name);
+    video[this.name] = this.value;
+}
+
 //Hook up the event listeners
 video.addEventListener('click', tooglePlay);
 video.addEventListener('play', updateButton);
@@ -42,7 +48,8 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', tooglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
-
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 
 
