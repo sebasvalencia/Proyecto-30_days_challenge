847bd6112d73847332b4a31c238e0533144e0e54
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-12-02 17:21:22 -0500

    Creamos funcion togglePlay

--- Dia 11 - Custom HTML5 Video Player/script.js
+++ Dia 11 - Custom HTML5 Video Player/script.js
@@ -8,7 +8,18 @@ const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 //Build out functions
-
+//Llamar a play o pause
+function tooglePlay(){
+    /*
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+    */
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
 
 //Hook up the event listeners
-
+video.addEventListener('click', tooglePlay);
