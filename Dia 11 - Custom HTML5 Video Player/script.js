22af19cce0439b93793f8a712f39f03ddb8e5eb7
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-12-02 17:14:05 -0500

    Obtener todos los elementos

--- Dia 11 - Custom HTML5 Video Player/script.js
+++ Dia 11 - Custom HTML5 Video Player/script.js
@@ -1,5 +1,11 @@
 //Get our elements
-
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
 
 //Build out functions
 
